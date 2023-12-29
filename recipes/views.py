fed063c0500cd3a46f339b617bd73a9733a23933
Eduardo Zelaya <eduardo.correia.zelaya@gmail.com>
2023-12-29 12:53:48 -0300

    Exemplo de template temp

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'recipes/home.html', context= {
+         'name': 'Eduardo Zelaya',
+    })
+
+def contato(request):
+    return render(request, 'me-apague/temp.html')
+ 
+def sobre(request):
+    return HttpResponse('SOBRE')
