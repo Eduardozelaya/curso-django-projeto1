818287e56128c8eca87dc4fe1c5412492bcd831c
Eduardo Zelaya <eduardo.correia.zelaya@gmail.com>
2024-01-22 10:08:17 -0300

    Teste Category e recipe detail carregam receita correta

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -49,6 +49,17 @@ class RecipeViewsTest(RecipeTestBase):
         )
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:category',args=(1,)))
+        content = response.content.decode('utf-8')
+
+        # Check if one recipe exists
+        self.assertIn(needed_title, content)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func,views.recipe)
@@ -59,5 +70,21 @@ class RecipeViewsTest(RecipeTestBase):
         )
         self.assertEqual(response.status_code, 404)
 
-    
+    def test_recipe_detail_template_loads_the_correct_recipe(self):
+        needed_title = 'This is detail page - It loads one recipe'
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(
+            reverse(
+                'recipes:recipe',
+                kwargs={
+                    'id': 1
+                }
+            )
+        )
+        content = response.content.decode('utf-8')
+
+        # Check if one recipe exists
+        self.assertIn(needed_title, content)
         
