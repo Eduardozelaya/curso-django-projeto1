0bd87ae2a4ae2e80a4e3084db7c60541efc9783f
Eduardo Zelaya <eduardo.correia.zelaya@gmail.com>
2024-01-27 14:00:04 -0300

    Correções de Teste

--- recipes/tests/test_recipe_model_category.py
+++ recipes/tests/test_recipe_model_category.py
@@ -11,10 +11,10 @@ class RecipeCategoryModelTest(RecipeTestBase):
     def test_recipe_category_model_string_representation_is_name_field(self):
         self.assertEqual(
             str(self.category),
-            self.category.name + ' '
+            self.category.name + ''
         )
 
     def test_recipe_category_model_name_max_length_is_65_chars(self):
-        self.category.name = 'A' * 65
+        self.category.name = 'A' * 66
         with self.assertRaises(ValidationError):
             self.category.full_clean()
