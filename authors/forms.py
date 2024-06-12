fe9bf9b32f21bed46dadef039eb82c242fa08700
Eduardo Zelaya <eduardo.correia.zelaya@gmail.com>
2024-06-12 20:27:21 -0300

    salvar alteracoes

--- authors/forms.py
+++ authors/forms.py
@@ -71,7 +71,7 @@ class RegisterForm(forms.ModelForm):
         help_text=(
             'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. The length '
-            'should be at least 8 characters'
+            'should be at least 8 characters.'
         ),
         validators=[strong_password],
         label='Password'
