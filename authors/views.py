8c601bf1eb6697fe9042c54e7089899827d2d1d8
Eduardo Zelaya <eduardo.correia.zelaya@gmail.com>
2024-08-05 22:37:30 -0300

    Dashboard de recipe edit

--- authors/views.py
+++ authors/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from recipes.models import Recipe
 
 # Create your views here.
 # def register_view(request):
@@ -106,4 +107,35 @@ def logout_view(request):
     
 @login_required(login_url='authors:login',redirect_field_name='next')
 def dashboard(request):
-    return render(request,'authors/pages/dashboard.html')
+    recipes = Recipe.objects.filter(
+        is_published = False,
+        author = request.user
+    )
+    return render(
+        request,
+        'authors/pages/dashboard.html',
+        context={
+        'recipes':recipes,
+        }
+    )
+    
+    
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_edit(request, id):
+    recipe = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+        pk=id,
+    )
+
+    if not recipe:
+        raise Http404()
+
+    return render(
+        request,
+        'authors/pages/dashboard_recipe.html',
+        context={
+
+        }
+    )
+
