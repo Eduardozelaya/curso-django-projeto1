a13fa20f8b0269ac938a094f7495f47f7276d451
Eduardo Zelaya <eduardo.correia.zelaya@gmail.com>
2024-10-26 11:55:26 -0300

    sincronizando

--- project/settings.py
+++ project/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get('DEBUG') == '1' else False
-
-ALLOWED_HOSTS = []
+#DEBUG = True if os.environ.get('DEBUG') == '1' else False
+DEBUG = 1
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
