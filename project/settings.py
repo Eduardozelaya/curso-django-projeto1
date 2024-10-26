5e68c397748c6be2a5222d5202d73f4b670d8645
Eduardo Zelaya <eduardo.correia.zelaya@gmail.com>
2024-10-26 10:02:58 -0300

    teste

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
-DEBUG = True
+DEBUG = 1
 
 ALLOWED_HOSTS = ['*']
 
