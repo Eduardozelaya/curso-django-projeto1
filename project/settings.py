338ca2bf634a5cd215385340328bd2724f695c8c
Eduardo Zelaya <eduardo.correia.zelaya@gmail.com>
2024-10-31 22:24:08 -0300

    Django debug toolbar

--- project/settings.py
+++ project/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     # A gente tem APPS também
     'recipes',
     'authors',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -153,3 +155,9 @@ MESSAGE_TAGS = {
     constants.SUCCESS: 'message-success',
     constants.WARNING: 'message-warning',
 }
+
+
+# Django Debug toolbar
+INTERNAL_IPS = [
+    '127.0.0.1',
+] 
