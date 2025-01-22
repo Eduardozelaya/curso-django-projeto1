b8ab60ab0f868cd09b3b5f748b5c6435456d6dc5
Eduardo Zelaya <eduardo.correia.zelaya@gmail.com>
2025-01-22 15:10:59 -0300

    Adicionando load_dotenv no settings.py

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+
+if os.environ.get('DEBUG',None) is None:
+    from dotenv import load_dotenv
+    load_dotenv()
+
 from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- recipes/views.py
+++ recipes/views.py
@@ -4,12 +4,14 @@ from django.db.models import Q, F, Value
 from recipes.models import Recipe
 from utils.pagination import make_pagination 
 from django.views.generic import DetailView,ListView
+from django.contrib.contenttypes.models import ContentType
 import os
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models.aggregates import Count,Max,Min,Avg,Sum
 from django.db.models.functions import Concat
+from tag.models import Tag
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
@@ -50,6 +52,7 @@ class RecipeListViewBase(ListView):
         )
 
         qs = qs.select_related('author','category')
+        qs = qs.prefetch_related('tags')
         return qs
     
     def get_context_data(self,*args, **kwargs):
@@ -109,6 +112,25 @@ class RecipeListViewSearch(RecipeListViewBase):
             'additional_url_query':f'&q={search_term}',
         })
         
+        return ctx
+    
+class RecipeListViewTag(RecipeListViewBase):
+    template_name = 'recipes/pages/tag.html'
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(tags__slug=self.kwargs.get('slug', ''))
+        return qs
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_title = Tag.objects.filter(
+            slug=self.kwargs.get('slug', '')
+        ).first()
+        if not page_title:
+            page_title = 'No recipes found'
+        page_title = f'{page_title} - Tag |'
+        ctx.update({
+            'page_title': page_title,
+        })
         return ctx
     
         
